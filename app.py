525f9a65075cacfcc598995bdab4adbd4ef4d01b
ttrice <ttrice.analyst@gmail.com>
2024-03-27 11:10:46 -0400

    Syncing test

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from basketball_reference_web_scraper import client
 import pandas as pd
+from flask import Flask, render_template, request
 
 #stats = client.players_advanced_season_totals(season_end_year=2024)
 stats = client.players_season_totals(season_end_year=2024)
 
 df = pd.DataFrame.from_dict(stats)
-
-print(df.columns)
+#print(df.columns)
 
 print(df
         .filter(['name','points'])
       .query("name.str.find('Kevin')>=0")
       .head(10)
       )
+
+app = Flask(__name__)
+
+@app.route("/")
+def web_app_home():
+    return render_template()
