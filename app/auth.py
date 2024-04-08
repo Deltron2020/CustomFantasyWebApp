5d931243a4683efb865e0f8d8ebc5a6e1efb8168
ttrice <ttrice.analyst@gmail.com>
2024-04-08 15:17:27 -0400

    added functionality to login & signup pages

--- app/auth.py
+++ app/auth.py
@@ -1,15 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return "<p>login</p>"
+    return render_template('FA_Login.html')
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        flash('Sign Up Successful!', category='success')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password1 = request.form.get('password')
+        password2 = request.form.get('confirm')
+        dob = request.form.get('dob')
+        promo = request.form.get('promo')
+
+        #data = request.form
+
     return render_template('FA_Signup.html')
